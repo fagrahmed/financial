2c543a99a950dbc5fb0e200551e8263d902d40e1
frefaat <frefaat@axispay.app>
2024-07-30 14:19:18 +0300

    correct td linking to tf

--- models/cohort_txn_profit.sql
+++ models/cohort_txn_profit.sql
@@ -10,28 +10,28 @@ SELECT
     replace(substring(wd.wallet_status,14) ,'_',' ') AS wallet_status,
     pd.partner_name,
     pd.profile_type,
-    tft.is_employee,
-    tft.txndetailsid,
-    tft.transaction_modifiedat_local,
+    tf.is_employee,
+    td.txndetailsid,
+    tf.transaction_modifiedat_local,
     ddm.full_date,
     tidm.full_time,
     td.txntype,
-    tft.amount,
-    tft.total_revenue_before_vat,
-    tft.total_cost_before_vat,
-    (tft.total_revenue_before_vat - tft.total_cost_before_vat) as profit_before_vat 
+    tf.amount,
+    tf.total_revenue_before_vat,
+    tf.total_cost_before_vat,
+    (tf.total_revenue_before_vat - tf.total_cost_before_vat) as profit_before_vat 
 
-FROM {{ source('dbt-facts', 'transactions_fact') }} tft
+FROM {{ source('dbt-facts', 'transactions_fact') }} tf
 JOIN {{ source('dbt-dimensions', 'wallets_dimension') }} wd
-    ON wd.id = tft.wallet_key
+    ON wd.id = tf.wallet_key
 JOIN {{ source('dbt-dimensions', 'profiles_dimension') }} pd
-    ON pd.id = tft.profile_key
+    ON pd.id = tf.profile_key
 JOIN {{ source('dbt-dimensions', 'transactions_dimension') }} td
-    ON td.txndetailsid = tft.txndetailsid
+    ON td.id = tf.txn_key
 JOIN {{ source('dbt-dimensions', 'date_dimension')}} ddm
-    ON tft.date_txn_modified_key = ddm.date_id
+    ON tf.date_txn_modified_key = ddm.date_id
 JOIN {{ source('dbt-dimensions', 'time_dimension')}} tidm
-    ON tft.time_txn_modifed_key = tidm.time_id
+    ON tf.time_txn_modifed_key = tidm.time_id
 WHERE wd.wallet_type = 'WalletType_CONSUMER'
     AND td.transactionstatus IN ('TransactionStatus_POSTED', 'TransactionStatus_POSTED_FAWRY','TransactionStatus_PENDING_ADVICE')
 
